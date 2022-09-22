8fdd8c90bdacdfcfa77cd3ac61fcc986a321fcf6
sharonren02 <91091242+sharonren02@users.noreply.github.com>
2022-09-21 22:40:22 -0400

    Update IndexPage.java

--- src/main/java/com/develogical/web/IndexPage.java
+++ src/main/java/com/develogical/web/IndexPage.java
@@ -7,7 +7,7 @@ public class IndexPage extends HtmlPage {
     @Override
     protected void writeContentTo(PrintWriter writer) {
         writer.println(
-                "<h1>Welcome too this webapp!</h1>" +
+                "<h1>Welcome to this webapp!</h1>" +
                 "<p>Enter your query in the box below: " +
                   "<form><input type=\"text\" name=\"q\" />" +
                     "<input type=\"submit\">" +
